38afe462d724e01c792a9055205ac1dc5e1b2539
Tobias Bohwalli <hi@futhr.io>
2026-09-09 22:28:34 +0200

    chore: configure release tooling

--- mix.exs
+++ mix.exs
@@ -48,6 +48,7 @@ defmodule WotexConformance.MixProject do
     [
       {:jason, "~> 1.4"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:git_ops, "~> 2.10", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:doctor, "~> 0.22", only: [:dev, :test], runtime: false},
       {:ex_check, "~> 0.16", only: [:dev, :test], runtime: false},
